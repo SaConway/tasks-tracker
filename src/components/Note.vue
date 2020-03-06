84412e77ffd36bb27d1c242ef60a4e714c57f17e
Sagee Conway <sageeconway@gmail.com>
2020-03-06 17:48:18 +0200

    Note component

---
+++ src/components/Note.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="note">
+    <button @click="deleteNote(index)" class="delete-note">
+      <svg viewBox="0 0 24 24">
+        <path
+          d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"
+        />
+      </svg>
+    </button>
+    <textarea v-model="note.text" v-autoresize-height></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["note"]
+};
+</script>
+
+<style scoped>
+textarea {
+  width: 100%;
+  color: var(--accent-color);
+  background-color: transparent;
+  font-size: var(--normal-font-size);
+  border: 1px solid var(--accent-color);
+  border-radius: var(--border-radius);
+  padding: 2.5rem 1rem;
+  resize: none;
+  overflow-y: hidden;
+}
+
+.delete-note {
+  position: absolute;
+  top: 5px;
+  left: 2px;
+  width: 25px;
+  height: 25px;
+  padding: 0;
+  border: none;
+}
+
+.delete-note svg {
+  height: 15px;
+  width: 15px;
+  fill: var(--accent-color);
+  transition: 0.3s transform ease-in-out;
+}
+
+.delete-note:hover svg {
+  transform: scale(1.05, 1.05);
+}
+</style>
--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,26 +1,23 @@
 <template>
   <div class="notes">
-    <div
+    <Note
       v-for="(note, index) in notes"
       :key="note.date"
+      :note="note"
       class="note"
       v-bind:style="{ animation: `fade-in 0.5s ${index * 0.3}s ease-in-out forwards` }"
-    >
-      <button @click="deleteNote(index)" class="delete-note">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-          <path
-            d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"
-          />
-        </svg>
-      </button>
-      <textarea v-model="note.text" v-autoresize-height></textarea>
-    </div>
+    />
   </div>
 </template>
 
 <script>
+import Note from "../components/Note";
+
 export default {
   props: ["notes"],
+  components: {
+    Note
+  },
   methods: {
     deleteNote(index) {
       window.Vue.delete(this.notes, index);
@@ -30,23 +27,6 @@ export default {
 </script>
 
 <style scoped>
-textarea {
-  padding: 0.5rem;
-  border-radius: var(--border-radius);
-  resize: none;
-  font-size: var(--normal-font-size);
-}
-
-button.delete-note {
-  position: absolute;
-  top: 5px;
-  left: 2px;
-  width: 25px;
-  height: 25px;
-  padding: 0;
-  border: none;
-}
-
 .notes {
   padding: 0 0 2.5rem 0;
   margin-top: 1rem;
@@ -64,26 +44,5 @@ button.delete-note {
   color: var(--accent-color);
   position: relative;
   opacity: 0;
-}
-
-.note textarea {
-  background-color: transparent;
-  color: var(--accent-color);
-  width: 100%;
-  padding: 2.5rem 1rem;
-  overflow-y: hidden;
-  border: 1px solid var(--accent-color);
-  border-radius: var(--border-radius);
-}
-
-.delete-note svg {
-  height: 15px;
-  width: 15px;
-  fill: var(--accent-color);
-  transition: 0.3s transform ease-in-out;
-}
-
-.delete-note:hover svg {
-  transform: scale(1.05, 1.05);
 }
 </style>
